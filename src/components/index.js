145f8ca26eb10c1281beb57bdcea9033e1333ec4
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-09-20 14:30:06 +0300

    All components moved components folder.

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Item from "./Item";
+import Stats from "./Stats";
+
+export { Logo, Form, PackingList, Item, Stats };
